b636e1b69d2a116996bab30bfcbe6c9ff9ddce8d
vishrutss <vishrutss@gmail.com>
2023-04-25 16:12:16 -0700

    Fixed cargo clippy error, added cargo fmt changes

--- rust_in_peace/src/main.rs
+++ rust_in_peace/src/main.rs
@@ -1,22 +1,22 @@
-use std::io;
 use regex::Regex;
+use std::io;
 
 fn main() {
-    println!("Hello, Player!");
-    println!("");
-    println!("Welcome to Rust In Peace");
-    println!("");
+    println!("Hello, Player!\n");
+    println!("Welcome to Rust In Peace\n");
     println!("Would you like to start the game? (Y/N)");
-    
+
     //https://www.geeksforgeeks.org/standard-i-o-in-rust/
-    let mut answer=String::new();
-    io::stdin().read_line(&mut answer).expect("failed to read input");
-    
+    let mut answer = String::new();
+    io::stdin()
+        .read_line(&mut answer)
+        .expect("failed to read input");
+
     //https://docs.rs/regex/latest/regex/
     let no = Regex::new("[nN]|[nN][oO]").unwrap();
 
     //https://doc.rust-lang.org/std/primitive.str.html#method.trim
-    if no.is_match(answer.trim()){
+    if no.is_match(answer.trim()) {
         println!("Goodbye!");
         std::process::exit(0);
     }
