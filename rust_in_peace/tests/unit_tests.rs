9ed2c648fc4c5ffdda7d9a52bb167b85bcf1d648
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-14 12:16:25 -0700

    added test and some formatting

--- rust_in_peace/tests/unit_tests.rs
+++ rust_in_peace/tests/unit_tests.rs
@@ -29,45 +29,47 @@ mod tests {
     }
 
     #[test]
-fn test_update_state() {
-    let mut world = World::new();
-    // Test case 1: Quit command
-    let command = Command::Quit;
-    let result = world.update_state(&command);
-    assert_eq!(result, "Quitting.\nThank you for playing!"); // Checking if the result matches the expected message
+    fn test_update_state() {
+        let mut world = World::new();
+        // Test case 1: Quit command
+        let command = Command::Quit;
+        let result = world.update_state(&command);
+        assert_eq!(result, "Quitting.\nThank you for playing!"); // Checking if the result matches the expected message
 
-    // Test case 2: Unknown command
-    let command = Command::Unknown("InvalidCommand".to_string());
-    let result = world.update_state(&command);
-    assert!(result.contains("Invalid command!!")); // Checking if the result contains the expected message
+        // Test case 2: Unknown command
+        let command = Command::Unknown("InvalidCommand".to_string());
+        let result = world.update_state(&command);
+        assert!(result.contains("Invalid command!!")); // Checking if the result contains the expected message
+    }
 
-}
+    #[test]
+    fn test_do_look() {
+        let mut world = World::new();
 
-#[test]
-fn test_do_look() {
-    let mut world = World::new();
+        // Set up the objects and player's location
+        world.objects[LOC_PLAYER].location = Some(LOC_FOREST);
+        world.objects[LOC_FOREST].label = vec!["Forest".to_string()];
+        world.objects[LOC_FOREST].description = "Look out for tree people.".to_string();
 
-    // Set up the objects and player's location
-    world.objects[LOC_PLAYER].location = Some(LOC_FOREST);
-    world.objects[LOC_FOREST].label = vec!["Forest".to_string()];
-    world.objects[LOC_FOREST].description = "Look out for tree people.".to_string();
+        // Test case 1: Look without specifying a noun
+        let result = world.do_look("");
+        let expected = " You are in the Forest\n Look out for tree people..\n\nYou see:\nA path to the north leading out of the forest leading to an old Tavern\n";
+        assert_eq!(result, expected);
 
-    // Test case 1: Look without specifying a noun
-    let result = world.do_look("");
-    let expected = " You are in the Forest\n Look out for tree people..\n\nYou see:\nA path to the north leading out of the forest leading to an old Tavern\n";
-    assert_eq!(result, expected);
-
-    // Test case 2: Look with an invalid noun
-    let result = world.do_look("invalid");
-    let expected = "Invalid command!!\n";
-    assert_eq!(result, expected);
-}
+        // Test case 2: Look with an invalid noun
+        let result = world.do_look("invalid");
+        let expected = "Invalid command!!\n";
+        assert_eq!(result, expected);
+    }
 
     #[test]
     fn test_do_go() {
         let mut world = World::new();
         let result = world.do_go(&"South".to_string());
-        assert_eq!(result, "You see nothing but trees. There is no other path in that direction.");
+        assert_eq!(
+            result,
+            "You see nothing but trees. There is no other path in that direction."
+        );
 
         let result = world.do_go(&"Invalid".to_string());
         assert_eq!(result, "Invalid command!!");
@@ -97,7 +99,10 @@ fn test_do_look() {
         // Test consuming an object
         let result = world.do_consume(Some(world.objects.len() - 1));
 
-        assert_eq!(result, "You have consumed the item. Your health has increased to 90\n");
+        assert_eq!(
+            result,
+            "You have consumed the item. Your health has increased to 90\n"
+        );
         assert_eq!(world.objects[LOC_PLAYER].health, Some(90));
         assert_eq!(world.objects[world.objects.len() - 1].location, None);
     }
@@ -111,14 +116,14 @@ fn test_do_look() {
         let object_index = world.objects.len();
         world.objects.push(Object {
             label: vec!["Sword".to_string()],
-                    description: "A rusty sword.".to_string(),
-                    location: Some(LOC_DUNGEONS),
-                    destination: None,
-                    item: Some(true),
-                    enemy: false,
-                    health: None,
-                    attack: Some(20),
-                    consumable: Some(false),
+            description: "A rusty sword.".to_string(),
+            location: Some(LOC_DUNGEONS),
+            destination: None,
+            item: Some(true),
+            enemy: false,
+            health: None,
+            attack: Some(20),
+            consumable: Some(false),
         });
 
         // Test dropping an object
@@ -127,7 +132,6 @@ fn test_do_look() {
         assert_eq!(result, "You are not holding any Sword.\n");
         assert_eq!(world.objects[object_index].location, Some(LOC_DUNGEONS));
     }
-
 
     #[test]
     fn test_player_here() {
@@ -146,5 +150,17 @@ fn test_do_look() {
         let result = world.player_here();
         assert_eq!(result, Some(LOC_FOREST));
     }
-}
 
+    #[test]
+    fn test_display_locations() {
+        let mut world = World::default();
+
+        // Set up the initial world state
+        world.objects[LOC_PLAYER].location = Some(LOC_PLAYER);
+
+        // Test the display of available locations
+        let expected_result = "Available locations:\n0: Forest\n1: Dungeons\n2: Cave\n3: Tavern\n4: Village\n5: Stronghold\n";
+        let result = world.display_locations();
+        assert_eq!(result, expected_result);
+    }
+}
